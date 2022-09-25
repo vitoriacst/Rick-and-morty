36dad438ebab86f07fb9939e41156ea3ed37c57b
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-09-25 15:46:53 -0300

    feat: :sparkles: Criando a página de personagens

---
+++ app/src/pages/Character/Character.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import UseAPi from '../../hooks/useApi';
+
+const Character = () => {
+  const { character } = UseAPi('https://rickandmortyapi.com/api/character');
+  console.log(character);
+
+  return (
+    <>
+      <div className="flex flex-wrap gap-6 p-6 justify-center">
+        {character.map((character) => {
+          return (
+            <div className="max-w-sm backdrop-blur-sm bg-white rounded-lg border border-black shadow-md dark:bg-gray-800 dark:border-gray-700">
+              <div className="card w-96 bg-base-100 shadow-xl">
+                <span className="indicator-item badge badge-primary">
+                  {character.status}
+                </span>
+                <figure>
+                  <img src={character.image} alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title text-blue-600">
+                    {character.name}
+                    <div className="badge badge-accent badge-outliney">
+                      {character.species}
+                    </div>
+                  </h2>
+                  <p></p>
+                  <div className="card-actions justify-end">
+                    <div className="badge badge-outline">
+                      {character.location.name}
+                    </div>
+                    <div className="badge badge-outline">
+                      {character.gender}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Character;
