9edf48797cbd847768ead5a5a6fd903bcf3ea63b
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-09-25 15:47:33 -0300

    refactor: :recycle: Refatorando a e mudando os nomes das variáveis

--- app/src/hooks/useApi.js
+++ app/src/hooks/useApi.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 
 const UseAPi = (url) => {
-  const [pokemons, setPokemons] = useState([]);
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setPokemons(data);
+        setCharacter(data.results);
       });
   }, []);
 
-  return { pokemons };
+  return { character };
 };
 
 export default UseAPi;
