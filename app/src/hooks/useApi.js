5b730958651b2798478d9f190d8e4836d2b707ea
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-09-25 23:02:57 -0300

    feat: :sparkles: Criando a página de episódios

--- app/src/hooks/useApi.js
+++ app/src/hooks/useApi.js
@@ -2,16 +2,18 @@ import { useEffect, useState } from 'react';
 
 const UseAPi = (url) => {
   const [character, setCharacter] = useState([]);
+  const [episode, setEpisode] = useState([]);
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setCharacter(data.results);
+        setEpisode(data.results);
       });
   }, []);
 
-  return { character };
+  return { character, episode };
 };
 
 export default UseAPi;
--- app/src/pages/Episodes.js/index.js
+++ app/src/pages/Episodes.js/index.js
@@ -1,8 +1,28 @@
+import Header from '../../components/header/Header';
+import UseAPi from '../../hooks/useApi';
+
 const Episode = () => {
+  const { episode } = UseAPi('https://rickandmortyapi.com/api/episode');
+  console.log(episode);
   return (
-    <div>
-      <h1>Episode</h1>
-    </div>
+    <>
+      <Header />
+      <h1 className="text-yellow-300 font-bold p-4 text-2xl">Episódios</h1>
+      <div className="flex flex-wrap gap-6 p-6 justify-center">
+        {episode.map((e) => {
+          return (
+            <div>
+              <div className="mockup-window border bg-base-300 w-96 ">
+                <div className="flex justify-center px-4 py-16 bg-base-100 flex-col items-center">
+                  <p className="font-bold">{e.name}</p>
+                  <p className="font-mono">{e.air_date}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
